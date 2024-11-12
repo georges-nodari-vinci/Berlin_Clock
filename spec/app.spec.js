3137154ac8d406e8e33378958897925da8cc7ff3
Melih Aksoy <melih.aksoy@student.vinci.be>
2024-11-12 21:50:11 +0100

    test: add unit test for test with 0 minute

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,3 +1,9 @@
-describe("Berlin Clock", () => {
-  // implement the complete test for Berlin Clock here
+import { getSimpleMinutesRowFromTimestamp } from "../app.js";
+
+describe("Berlin Clock - Simple Hours Row from Timestamp", () => {
+  it("should display 0 lamps off for 0 minutes", () => {
+    const timestamp = new Date("2024-11-11T00:00:00").getMinutes(); 
+    expect(getSimpleMinutesRowFromTimestamp(timestamp)).toEqual("OOOO");
+  });
+
 });
