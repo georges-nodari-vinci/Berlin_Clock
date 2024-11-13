2fef29785f03572ee2f78bad588bfb3aaed869f3
vaeli-uku <uku.vaeli.001@student.uni.lu>
2024-11-13 20:40:04 +0100

    more commentary

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -5,27 +5,27 @@ import { getHighHoursRowFromTimestamp } from "../app.js";
 
 describe("Berlin Clock - Simple Hours Row from Timestamp", () => {
   it("should display 0 lamps off for 0 hours", () => {
-    const timestamp = new Date("2024-11-11T00:00:00"); 
+    const timestamp = new Date("2024-11-11T00:00:00"); //00:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("OOOO");
   });
 
   it("should display 1 lamp on for 5 minute", () => {
-    const timestamp = new Date("2024-11-11T05:00:00"); 
+    const timestamp = new Date("2024-11-11T05:00:00"); //05:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("YOOO");
   });
 
   it("should display 2 lamps on for 10 hours", () => {
-    const timestamp = new Date("2024-11-11T10:00:00"); 
+    const timestamp = new Date("2024-11-11T10:00:00"); //10:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("YYOO");
   });
 
   it("should display 3 lamps on for 15 hours", () => {
-    const timestamp = new Date("2024-11-11T15:00:00"); 
+    const timestamp = new Date("2024-11-11T15:00:00"); //15:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("YYYO");
   });
 
   it("should display 4 lamps on for 20 hours", () => {
-    const timestamp = new Date("2024-11-11T20:00:00");
+    const timestamp = new Date("2024-11-11T20:00:00");//20:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("YYYY");
   });
 });
