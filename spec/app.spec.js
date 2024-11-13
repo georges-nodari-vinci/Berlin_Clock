f457cda8520cdb22d81a077205943b1106b64765
Melih Aksoy <melih.aksoy@student.vinci.be>
2024-11-13 17:50:02 +0100

    test: correct describe block for Simple Minutes Row from Timestamp

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,6 +1,6 @@
 import { getSimpleMinutesRowFromTimestamp } from "../app.js";
 
-describe("Berlin Clock - Simple Hours Row from Timestamp", () => {
+describe("Berlin Clock - Simple minutes Row from Timestamp", () => {
   it("should display 0 lamps off for 0 minutes", () => {
     const timestamp = new Date("2024-11-11T00:00:00"); 
     expect(getSimpleMinutesRowFromTimestamp(timestamp)).toEqual("OOOO");
