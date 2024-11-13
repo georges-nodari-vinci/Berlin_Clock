86968adbd4bd5a22212f4e24b556acc8971d5ecc
vaeli-uku <uku.vaeli.001@student.uni.lu>
2024-11-13 20:47:19 +0100

    test text fix

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -3,13 +3,13 @@
 
 import { getHighHoursRowFromTimestamp } from "../app.js";
 
-describe("Berlin Clock - Simple Hours Row from Timestamp", () => {
+describe("Berlin Clock - 5 Hours Row from Timestamp", () => {
   it("should display 0 lamps off for 0 hours", () => {
     const timestamp = new Date("2024-11-11T00:00:00"); //00:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("OOOO");
   });
 
-  it("should display 1 lamp on for 5 minute", () => {
+  it("should display 1 lamp on for 5 hours", () => {
     const timestamp = new Date("2024-11-11T05:00:00"); //05:00
     expect(getHighHoursRowFromTimestamp(timestamp)).toEqual("YOOO");
   });
