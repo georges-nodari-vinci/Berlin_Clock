90d2d9e8eac5113f334fb2bc1124b0c286308712
Georges Nodari <ngeorges.dev@gmail.com>
2024-11-13 20:55:11 +0100

    feat: add module type to package.json and update test timestamps in app.spec.js

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,13 +1,11 @@
-describe("Berlin Clock", () => {
-  // implement the complete test for Berlin Clock here
-});
+import { getSecondsLamp } from "../app.js";
 
 describe("Berlin Clock - Seconds Lamp", () => {
   it('should return "R" when the seconds of the given timestamp are even, indicating the lamp is on', () => {
     const evenTimestamps = [
-      new Date("2024-11-13T00:00:00"), // 0 seconds
-      new Date("2024-11-13T00:00:02"), // 2 seconds
-      new Date("2024-11-13T00:00:04"), // 4 seconds
+      new Date("2024-11-11T00:00:00"), // 0 seconds
+      new Date("2024-11-11T00:00:02"), // 2 seconds
+      new Date("2024-11-11T00:00:04"), // 4 seconds
     ];
 
     evenTimestamps.forEach((timestamp) => {
@@ -17,9 +15,9 @@ describe("Berlin Clock - Seconds Lamp", () => {
 
   it('should return "O" when the seconds of the given timestamp are odd, indicating the lamp is off', () => {
     const oddTimestamps = [
-      new Date("2024-11-13T00:00:01"), // 1 second
-      new Date("2024-11-13T00:00:03"), // 3 seconds
-      new Date("2024-11-13T00:00:05"), // 5 seconds
+      new Date("2024-11-11T00:00:01"), // 1 second
+      new Date("2024-11-11T00:00:03"), // 3 seconds
+      new Date("2024-11-11T00:00:05"), // 5 seconds
     ];
 
     oddTimestamps.forEach((timestamp) => {
