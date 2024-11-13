72b1adb58ff73b7617e52b9da22625acb6614173
Georges Nodari <ngeorges.dev@gmail.com>
2024-11-13 20:49:01 +0100

    test: add test for odd seconds in getSecondsLamp function to verify lamp state

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -14,4 +14,16 @@ describe("Berlin Clock - Seconds Lamp", () => {
       expect(getSecondsLamp(timestamp)).toBe("R");
     });
   });
+
+  it('should return "O" when the seconds of the given timestamp are odd, indicating the lamp is off', () => {
+    const oddTimestamps = [
+      new Date("2024-11-13T00:00:01"), // 1 second
+      new Date("2024-11-13T00:00:03"), // 3 seconds
+      new Date("2024-11-13T00:00:05"), // 5 seconds
+    ];
+
+    oddTimestamps.forEach((timestamp) => {
+      expect(getSecondsLamp(timestamp)).toBe("O");
+    });
+  });
 });
