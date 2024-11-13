b2364657e6becf94cbf0e60361747b55bebde62e
vaeli-uku <uku.vaeli.001@student.uni.lu>
2024-11-13 20:21:38 +0100

    etape4branch

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 export function convertToBerlinClock() {
   // implement the complete code Berlin Clock here
 }
+
+
+
+
+export function getHighHoursRowFromTimestamp(timestamp) {
+  const hours = timestamp.getHours();
+  const onLamps = Math.floor(hours / 5); 
+
+  return "R".repeat(onLamps).padEnd(4, "O");
+}
